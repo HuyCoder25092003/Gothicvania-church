1fad2368221adde2b30373fc7eb53e62fa967464
HuyCoder25092003 <huylaptrinh6diem@gmail.com>
2023-06-20 17:59:35 +0700

    Update

--- Assets/00 SCRIPTS/GameManager.cs
+++ Assets/00 SCRIPTS/GameManager.cs
@@ -3,19 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
-    [SerializeField]GAMESTATE gameState;
+    [SerializeField] GAMESTATE gameState;
     public GAMESTATE GameState { get => gameState; set => gameState = value; }
     // Start is called before the first frame update
     void Start()
     {
-        PlayButton play= new PlayButton();
+        PlayButton play = new PlayButton();
         play.playButton();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
 }
